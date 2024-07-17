a61d2fe5569af4e27c52b2b9a98bf4baca0e45c0
IanEzeP <pereyra.ian00@gmail.com>
2024-07-17 01:41:46 -0300

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'PPS-Credito',
+  appId: 'pps.iep.credito',
+  appName: 'Carga Credito',
   webDir: 'www'
 };
 
