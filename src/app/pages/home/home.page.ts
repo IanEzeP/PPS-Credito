b0dd53877947fa6576397986605dc2f864d78ebe
IanEzeP <pereyra.ian00@gmail.com>
2024-07-17 19:48:09 -0300

    Libreria de lector de QR añadida

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-
+import { 
+  BarcodeScanner,
+  Barcode 
+} from '@capacitor-mlkit/barcode-scanning';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  public scannedData: string = '';
+
+  public barcodes: Barcode[] = []; //?
+  public infoQR: string | null = null;
+  public scanSupported: boolean = false;
+  public scanAvailable: boolean = false;
 
   constructor() {}
 
